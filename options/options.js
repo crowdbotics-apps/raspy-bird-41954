084f675faacec4c31cef4ae286e4d4425ee2d97d
Crowdbotics <team@crowdbotics.com>
2023-07-06 21:33:40 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "raspy-bird-41954",
+  url: "https://raspy-bird-41954.botics.co",
+  api: "https://raspy-bird-41954.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
